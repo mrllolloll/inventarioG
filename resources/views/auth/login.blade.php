785fdcc7c2f44e5d226c45386d2c3951b7604d7a
FidMarcano <fidelmedinam2@gmail.com>
2017-05-22 05:05:37 -0430

    Login arreglado

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -3,18 +3,18 @@
 @section('content')
 <div class="container">
     <div class="row">
-        <div class="col-md-8 col-md-offset-2" style="margin-top: 20px">
-            <div  class="panel panel-default">
+        <div class="col-md-8 col-md-offset-2" id="contenedor-login" >
+            <div  class="panel panel-default" id="contenedor-login1">
                <div class="panel-heading" id="login">Login</div>
                 <div class="panel-body" style="background-color:#f9fbff"> 
                     <form class="form-horizontal" role="form" method="POST" action="<?php echo e(url('/login')); ?>">
                         <?php echo e(csrf_field()); ?>
 
                         <div class="form-group<?php echo e($errors->has('email') ? ' has-error' : ''); ?>" >
-                            <label for="email" class="col-md-4 control-label">E-Mail Address</label>
+                            <label for="email" class="col-md-4 control-label" id="leyenda-login">E-Mail Address</label>
 
                             <div class="col-md-6">
-                                <input id="email" type="email" class="form-control" name="email" value="<?php echo e(old('email')); ?>" required autofocus>
+                                <input id="email"  type="email" class="form-control login-input" name="email" value="<?php echo e(old('email')); ?>" required autofocus>
 
                                 <?php if($errors->has('email')): ?>
                                     <span class="help-block">
@@ -24,10 +24,10 @@
                             </div>
                         </div>
                         <div class="form-group<?php echo e($errors->has('password') ? ' has-error' : ''); ?>">
-                            <label for="password" class="col-md-4 control-label">Password</label>
+                            <label for="password" class="col-md-4 control-label" id="leyenda-login">Password</label>
 
                             <div class="col-md-6">
-                                <input id="password" type="password" class="form-control" name="password" required>
+                                <input id="password" type="password" class="form-control login-input" name="password" required>
 
                                 <?php if($errors->has('password')): ?>
                                     <span class="help-block">
